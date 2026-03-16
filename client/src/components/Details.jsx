7f71ef4d7915f765c0f0fb757a7cb31a0bcd52df
Haim Daniela Flalo <kvim.hda@gmail.com>
2026-03-16 14:57:59 +0200

    details finish

---
+++ client/src/components/Details.jsx
@@ -0,0 +1,32 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+function Details() {
+  const [launcher, setLauncher] = useState({});
+  async function getLauncherById() {
+    try {
+      const result = await axios.get("http://localhost:3000/api/launchers/id", {
+        headers: {
+          id: localStorage.getItem("id"),
+        },
+      });
+      setLauncher(result.data);
+    } catch (error) {
+      console.error("an error occured", error);
+    }
+  }
+  useEffect(() => {
+    getLauncherById();
+  }, []);
+  return (
+    <ul>
+      <li>id:{launcher._id}</li>
+      <li>name:{launcher.name}</li>
+      <li>latitude:{launcher.latitude}</li>
+      <li>longitude:{launcher.longitude}</li>
+      <li>city:{launcher.city}</li>
+    </ul>
+  );
+}
+
+export default Details;

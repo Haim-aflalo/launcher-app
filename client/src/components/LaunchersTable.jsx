4dfe087008b7f068abf3157cd6dc7e94bcbc8cbe
Haim Daniela Flalo <kvim.hda@gmail.com>
2026-03-16 15:47:04 +0200

    filter

--- client/src/components/LaunchersTable.jsx
+++ client/src/components/LaunchersTable.jsx
@@ -5,6 +5,7 @@ import { useNavigate } from "react-router-dom";
 function LaunchersTable() {
   const navigate = useNavigate();
   const [city, setCity] = useState("");
+  const [rocketType, setRocketType] = useState("");
   const [launchers, setLaunchers] = useState([]);
 
   async function fetchLaunchers() {
@@ -22,7 +23,8 @@ function LaunchersTable() {
   }
   useEffect(() => {
     fetchLaunchers();
-  }, [launchers]);
+    console.log(rocketType);
+  }, [rocketType, city]);
 
   return (
     <div>
@@ -31,13 +33,34 @@ function LaunchersTable() {
         placeholder="filter by city"
         onChange={(e) => setCity(e.target.value)}
       />
+      <input
+        type="text"
+        placeholder="filter by type"
+        onChange={(e) => setRocketType(e.target.value)}
+      />
+      {/* <select
+        name="reocketType"
+        id="rocketType"
+        onChange={(e) => setRocketType(e.target.value)}
+      >
+        <option value="">Choose a rocket type</option>
+        <option value="Shahab3">Shahab3</option>
+        <option value="Fetah110">Fetah110</option>
+        <option value="Radwan">Radwan</option>
+        <option value="Kheibar">Kheibar</option>
+      </select> */}
       <ul>
         {launchers
           .filter((launcher) => launcher.city === city || city === "")
+          .filter(
+            (launcher) =>
+              launcher.rocketType === rocketType || rocketType === "",
+          )
           .map((launcher) => (
             <li>
               <p>Name:{launcher.name}</p>
               <p>City:{launcher.city}</p>
+              <p>rocket type:{launcher.rocketType}</p>
               <button onClick={() => navToDetails(launcher._id)}>
                 Get details
               </button>

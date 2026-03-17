857cf2d754d5f67befdfd38c2066ad8780197ced
Haim Daniela Flalo <kvim.hda@gmail.com>
2026-03-17 12:36:05 +0200

    correction on server

--- server/routes/usersRoutes.js
+++ server/routes/usersRoutes.js
@@ -4,6 +4,8 @@ import {
   removeUserController,
   loginController,
   updateUserController,
+  getLastConnectedUserController,
+  getAllUsersControllers,
 } from "../controllers/usersControllers.js";
 import { checkToken } from "../middleware/checkToken.js";
 export const usersRouter = express.Router();
@@ -12,3 +14,5 @@ usersRouter.post("/register/create", checkToken, addUserController);
 usersRouter.post("/login", loginController);
 usersRouter.delete("/register/delete/:id", checkToken, removeUserController);
 usersRouter.put("/register/update", checkToken, updateUserController);
+usersRouter.get("/getUser", checkToken, getLastConnectedUserController);
+usersRouter.get("/getAllUser", checkToken, getAllUsersControllers);
